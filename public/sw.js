04d546a656715743b8990eabaa04d0f97a62652a
Team Services <abdulwahab10101c@gmail.com>
2025-07-25 09:14:43 +0000

    install button show in header

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,17 @@
-
+// This service worker is intentionally left blank for now.
+// It's required for a web app to be installable.
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+  console.log('Service worker installing...');
+  // Add an empty fetch listener to make the PWA installable.
+  self.addEventListener('fetch', () => {});
 });
 
 self.addEventListener('fetch', (event) => {
-  // We are not adding any offline functionality here.
-  // This is just to satisfy the PWA criteria.
-  event.respondWith(fetch(event.request));
+    // Basic network-first strategy
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            // If the network fails, you could try to serve from cache if you have one.
+            // For now, it will just result in a network error page.
+        })
+    );
 });
