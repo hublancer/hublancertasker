8fc279756a0773e8866a313ccb6fa97ed286e410
Team Services <abdulwahab10101c@gmail.com>
2025-07-25 08:20:34 +0000

    pwa work like other mobile apps , pwa load home page and other pages wit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -22,16 +22,18 @@ const auth = getAuth(app);
 const storage = getStorage(app);
 
 // Enable Firestore persistence
-enableIndexedDbPersistence(db)
-  .catch((err) => {
-    if (err.code == 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled in one tab at a time.
-      console.warn('Firestore persistence failed: multiple tabs open.');
-    } else if (err.code == 'unimplemented') {
-      // The current browser does not support all of the features required to enable persistence
-      console.warn('Firestore persistence is not available in this browser.');
-    }
-  });
+if (typeof window !== 'undefined') {
+  enableIndexedDbPersistence(db)
+    .catch((err) => {
+      if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled in one tab at a time.
+        console.warn('Firestore persistence failed: multiple tabs open.');
+      } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the features required to enable persistence
+        console.warn('Firestore persistence is not available in this browser.');
+      }
+    });
+}
 
 
 export { app, db, auth, storage };
