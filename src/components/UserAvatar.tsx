9cb01b168fe93f74f69650783dbbee2f93b13e4c
Team Services <abdulwahab10101c@gmail.com>
2025-07-25 05:46:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -3,7 +3,6 @@
 
 import { cn } from '@/lib/utils';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Timestamp } from 'firebase/firestore';
 
 interface UserAvatarProps {
   name?: string;
